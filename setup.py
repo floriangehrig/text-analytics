0067387027e0fde60cf316726d32f7cbc0074ee6
floriangehrig <46907562+floriangehrig@users.noreply.github.com>
2019-11-21 10:09:12 +0100

    Update

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="textanalytics", # Replace with your own username
+    version="0.0.1",
+    author="Florian Gehrig",
+    author_email="",
+    description="A package to quickly analyze text.",
+    url="https://github.com/floriangehrig/textanalytics",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
+
